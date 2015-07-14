d5dd9bc58a6a0e3f71c47625c8452c8e78fde3b7
Gabriele Petronella <gabriele@buildo.io>
2015-07-14 15:54:48 +0200

    Initial commit

---
+++ Instabug-CocoaPodsTests/Instabug_CocoaPodsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Instabug_CocoaPodsTests.swift
+//  Instabug-CocoaPodsTests
+//
+//  Created by Gabriele Petronella on 7/14/15.
+//  Copyright (c) 2015 buildo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Instabug_CocoaPodsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
